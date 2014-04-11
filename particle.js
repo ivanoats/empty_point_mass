4d73b338cdb9ca785db02fa87ca685314b119abc
Ivan Storck <ivanoats@gmail.com>
2014-04-11 14:26:17 -0700

    using vanilla js

--- particle.js
+++ particle.js
@@ -1,4 +1,29 @@
 'use strict';
-(function($) {
+document.addEventListener('DOMContentLoaded', function(){
+  var canvas = document.getElementById('canvas1');
+  var context = canvas.getContext('2d');
 
-})(jQuery);
+  var Point = function(x, y){
+    var point = {
+      position: {
+        x: x,
+        y: y
+      },
+      velocity: {
+        x: 0.0,
+        y: 0.0
+      },
+      forces: [],
+      drawPoint: function() {
+        context.fillStyle = '#fff';
+        context.fillRect(this.position.x, this.position.y, 2, 2);
+      },
+      updatePoint: function() {
+
+      }
+    }
+    return point;
+  }
+  context.fillStyle = '#000';
+  context.fillRect(0,0, canvas.width, canvas.height);
+});
